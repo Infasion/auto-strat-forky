f954a7a98478901c3cf6fe192b9dcb86bb81b0d6
KingLuc12 <89903214+KingLuc12@users.noreply.github.com>
2025-12-31 11:24:59 +0000

    chore: added matchmaking map for difficulty modes
    Added matchmaking map for different game modes, easier to update if / once TDS releases a new map.
    
    Also in shared table for addons.

--- Library.lua
+++ Library.lua
@@ -62,7 +62,12 @@ local ItemNames = {
 -- // tower management core
 local TDS = {
     placed_towers = {},
-    active_strat = true
+    active_strat = true,
+    matchmaking_map = {
+        ["Hardcore"] = "hardcore",
+        ["Pizza Party"] = "halloween",
+        ["Polluted"] = "polluted"
+    }
 }
 
 local upgrade_history = {}
@@ -615,28 +620,24 @@ function TDS:Mode(difficulty)
     local res
         repeat
             local ok, result = pcall(function()
-                if difficulty == "Hardcore" then
-                    return remote:InvokeServer("Multiplayer", "v2:start", {
-                        mode = "hardcore",
+                local mode = TDS.matchmaking_map[difficulty]
+
+                local payload
+
+                if mode then
+                    payload = {
+                        mode = mode,
                         count = 1
-                    })
-                elseif difficulty == "Pizza Party" then
-                    return remote:InvokeServer("Multiplayer", "v2:start", {
-                        mode = "halloween",
-                        count = 1
-                    })
-                elseif difficulty == "Polluted" then
-                    return remote:InvokeServer("Multiplayer", "v2:start", {
-                        mode = "polluted",
-                        count = 1
-                    })
+                    }
                 else
-                    return remote:InvokeServer("Multiplayer", "v2:start", {
+                    payload = {
                         difficulty = difficulty,
                         mode = "survival",
                         count = 1
-                    })
+                    }
                 end
+
+                return remote:InvokeServer("Multiplayer", "v2:start", payload)
             end)
 
             if ok and check_res_ok(result) then
